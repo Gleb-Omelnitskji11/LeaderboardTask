f4b6e5e16b47825ee1a1b2d19e89c6c2ad7d3135
Glib <omelniskij@gmail.com>
2025-10-24 20:48:19 +0300

    Struct complete

---
+++ Assets/Scripts/Leaderboard/PlayerModel.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public sealed class PlayerModel
+{
+   public string playerName;
+   public int score;
+   public string avatar;
+   public string type;
+}
